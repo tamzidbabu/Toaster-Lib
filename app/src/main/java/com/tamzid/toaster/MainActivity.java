8cd03277dae016c249169024f7f102cdecec6cc1
tamzid <tamzidbabu01@gmail.com>
2020-09-07 22:17:58 +0600

    project initiated

---
+++ app/src/main/java/com/tamzid/toaster/MainActivity.java
@@ -0,0 +1,18 @@
+package com.tamzid.toaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tamzid.toasterlib.ToasterMessage;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ToasterMessage.s(this,"Hello");
+    }
+}
